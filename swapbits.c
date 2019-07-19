332fbd6d2a56405d0286cac5bdcf0942a19301d9
grip90 <46645381+grip90@users.noreply.github.com>
2019-07-19 10:38:13 +0530

    Update swapbits.c
    generic code.

--- swapbits.c
+++ swapbits.c
@@ -1,5 +1,7 @@
 /*Swap bits 6 and 7 of a 32 bit variable*/
 #include <stdio.h>
+#define XPOS 6
+#define YPOS 7
 #define GETBIT(i,input) (((1<<i)&input)>>i)
 #define ZEROBIT(i,input) ((~(1<<i))&input)
 int A = 0x12345678;
@@ -10,7 +12,7 @@ int main() {
     //swap bits 6 and 7
     // A = 0001 0010 0011 0100 0101 0110 1011 1000
     //A = 0x123456B8
-A = ((ZEROBIT(6,A) & ZEROBIT(7,A)) | (GETBIT(7,A)<<6) | GETBIT(6,A)<<7);
+A = ((ZEROBIT(XPOS,A) & ZEROBIT(YPOS,A)) | (GETBIT(XPOS,A)<<YPOS) | GETBIT(YPOS,A)<<XPOS);
 
 printf("%0x",A);
 }
